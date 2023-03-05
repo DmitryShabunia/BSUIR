612173dfef51ec805f02ca28947fadd4946f2c75
Dmitry Shabunia <bulbasaurus01@gmail.com>
2023-03-05 18:01:39 +0300

    1

---
+++ PoHLL/Lab1/src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * The program "Guess of number" implements the algorithm
+ * of making a random number by the computer
+ * and guessing it by the user.
+ *
+ * Implemented graphical user interface (GUI)
+ * using the library to create a graphical
+ * interface Swing, and logical component.
+ */
+
+class Main{
+    /**
+     * This is the main method which run the game
+     * @param args Border of randomizing.
+     */
+    public static void main (String[] args) {
+        int border = Integer.parseInt(args[0]);
+        new MyGUI().runGUI(border);
+    }
+}
